69879147ca6533cd5f886b321276d48c62dd8c5d
Kosi Akporji <kosiazom@gmail.com>
2020-04-17 19:38:34 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,9 +1,9 @@
 def find_min_value(array)
  count = 0
- min_value = 0 
+ min_value = 2
  
  while count < array.length do 
-   if min_value < array[count]
+   if min_value > array[count]
      min_value = array[count]
  end
  count +=1
